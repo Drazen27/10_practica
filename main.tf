6e64263aeecd8b9421c0e1e4117a37543b8f8331
Drazen27 <103655456+Drazen27@users.noreply.github.com>
2025-12-01 20:38:12 -0400

    Destruir

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 
-resource "azurerm_resource_group" "terraform_rg" {
-  name     = "${var.name}-rg"
-  location = var.location
-  tags     = var.common_tags
-}
-resource "azurerm_virtual_network" "terraform_vnet" {
-  name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.terraform_rg.name
-  location            = var.location
-  address_space       = var.address_space
-  tags                = var.common_tags
-}
+# resource "azurerm_resource_group" "terraform_rg" {
+#   name     = "${var.name}-rg"
+#   location = var.location
+#   tags     = var.common_tags
+# }
+# resource "azurerm_virtual_network" "terraform_vnet" {
+#   name                = var.vnet_name
+#   resource_group_name = azurerm_resource_group.terraform_rg.name
+#   location            = var.location
+#   address_space       = var.address_space
+#   tags                = var.common_tags
+# }
--- variables.tf
+++ variables.tf
@@ -3,19 +3,23 @@ variable "location" {
   type        = string
   default     = "eastus"
 }
+
 variable "name" {
   description = "Nombre generico"
   type        = string
   default     = "drazen"
 }
+
 variable "common_tags" {
   description = "Common tags"
   type        = map(string)
 }
+
 variable "vnet_name" {
   description = "vnet name"
   type        = string
 }
+
 variable "address_space" {
   description = "address space"
   type        = list(string)
